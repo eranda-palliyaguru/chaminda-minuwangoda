247278fd19d3770e854aec62585d8cdf0786a5bb
eranda-palliyaguru <erandasampath2000@gmail.com>
2021-01-19 15:54:23 +0530

    up

--- sales_rp_print.php
+++ sales_rp_print.php
@@ -51,7 +51,7 @@ $sec = "1";
                   <th>Part Price</th>
 
 				  <th>Amount</th>
-                
+
                 </tr>
 
                 </thead>
@@ -212,7 +212,7 @@ $sec = "1";
                 </tr>
 					<tr>
 					<th>Total</th>
-                  <th>Rs.<?php echo $total; ?>.00</th>
+                  <th>Rs.<?php echo $total=$total=$advance; ?>.00</th>
                 </tr>
 					<tr>
 					<th>-</th>
